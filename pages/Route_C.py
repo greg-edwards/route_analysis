ab3d6c570a8963bd30db9185433b9b3bd5fe5501
gedwards880 <gedwards880@gmail.com>
2023-08-23 09:55:07 +1200

    Signed-off-by: gedwards880 <gedwards880@gmail.com>

--- pages/Route_C.py
+++ pages/Route_C.py
@@ -1,10 +1,10 @@
 import streamlit as st
+import folium
 from folium.features import Marker, Popup
 import pandas as pd
 import numpy as np
 import geopandas as gpd
 import leafmap
-import folium
 from streamlit_folium import st_folium
 
 st.set_page_config(layout="wide")
